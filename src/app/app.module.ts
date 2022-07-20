37380a66c7ccadaa3ff9c58a89c97b39addbbcf5
tranhung0609.github.io <trvanhung.nhk.0609@gmail.com>
2022-07-20 10:19:27 +0700

    not done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import { BookComponent } from './components/book/book.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/book.service.ts
@@ -0,0 +1,37 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Book} from "../models/book";
+
+const API_URL = 'http://localhost:3001/books'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookService {
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getAllBooks(): Observable<Book[]> {
+    return this.httpClient.get<Book[]>(API_URL);
+  }
+
+  getBookById(id: number): Observable<Book>{
+    return this.httpClient.get<Book>(API_URL + '/' + id);
+  }
+
+  createBook(book: Book): Observable<any> {
+    return this.httpClient.post<any>(API_URL, book);
+  }
+
+  updateBook(id: number, book: Book): Observable<any> {
+    return this.httpClient.put(`${API_URL}/${id}`, book);
+  }
+
+  // deleteBook(id: number): Observable<any> {
+  //   return this.httpClient.delete<any>(API_URL + '/' + id);
+  // }
+
+
+}
